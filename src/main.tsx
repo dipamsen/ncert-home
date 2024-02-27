8805206d85a11343a7fc6bb294e83ce5235965a3
Dipam Sen <dipamsen.fp@gmail.com>
2024-02-27 20:30:39 +0530

    Add footer with creator information

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import {
+  Box,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  Link,
+  createTheme,
+} from "@mui/material";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -18,6 +25,15 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <RouterProvider router={router} />
+      {/* footer (created by dipamsen) */}
+      <Box sx={{ marginTop: "20px", backgroundColor: "#242424", py: "10px" }}>
+        <Typography variant="subtitle1" align="center">
+          created by{" "}
+          <Link href="https://github.com/dipamsen" target="_blank">
+            dipamsen
+          </Link>
+        </Typography>
+      </Box>
     </ThemeProvider>
   </React.StrictMode>
 );
